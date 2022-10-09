80767dc90b9dd262e0d36c907c87a7a5061e8821
VoloshinArtur <115408461+VoloshinArtur@users.noreply.github.com>
2022-10-09 17:45:27 +0300

    решение задачи

---
+++ 111.cpp
@@ -0,0 +1,16 @@
+﻿//Voloshin AO
+//Task 1
+//09.10.2022
+#include <iostream>
+using namespace std;
+int main() {
+	int w;
+	cin >> w;
+	if (w % 2 == 0 && w!=2) {
+		cout << "YES";
+	}
+	else
+	{
+		cout << "NO";
+	}
+}
